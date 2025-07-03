2c92c0b6aea554f3eb677f07d55826f3f53d9644
stropee <stropee@mail.com>
2025-07-03 13:58:26 +0200

    mod 2 tp quiz partie 2

--- module_2/tp/script.js
+++ module_2/tp/script.js
@@ -1,16 +1,14 @@
 import {Question} from "./question.js";
-// import {Quiz} from "./quiz.js";
-//
-// let quiz = new Quiz()
+import {Quiz} from "./quiz.js";
+
+let quiz = new Quiz()
 let q1 = new Question("ca va ?", ['oui', 'non', 'je ne sais pas'], 2)
 let q2 = new Question("Pain au chocolat ou chocolatine ?", ['Pain au chocolat', 'chocolatine', 'je ne sais pas'], 0)
 
-document.body.append(q1.display())
+quiz.addQuestion(q1)
+quiz.addQuestion(q2)
+quiz.displayQuestion(q1)
 
-// quiz.addQuestion(q1)
-// quiz.addQuestion(q2)
-// quiz.displayQuestion(q1)
-//
-// document.getElementById('submit').addEventListener('click', function () {
-//     quiz.collectAnswer()
-// })
+document.getElementById('submit').addEventListener('click', function () {
+    quiz.collectAnswer()
+})
